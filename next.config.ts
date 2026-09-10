0de09156f3dfc3ce09027307125610b630f40cc4
PongsatornR-11 <basnitro41@gmail.com>
2026-09-10 21:59:26 +0700

    fix: allow images.unsplash.com and wildcard storage in next.config images.remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -11,11 +11,35 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
         hostname: "img.clerk.com",
       },
       {
         protocol: "https",
+        hostname: "*.supabase.co",
+      },
+      {
+        protocol: "https",
         hostname: "hiivzcinvtszjzqzyisf.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn4.iconfinder.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
   },
